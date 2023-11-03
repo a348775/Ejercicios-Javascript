173682502a6c58787a34625ef56962663189df6f
a348775 <134162225+a348775@users.noreply.github.com>
2023-11-02 20:21:12 -0600

    32.- Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista debiendo ir cada uno en una fila distinta.

---
+++ pr32.js
@@ -0,0 +1,9 @@
+const nombres = [
+  'Juan', 'Camaney', 'Carlos', 'Poncho', 'Lupe', 'Ramiro', 'Miguel', 'Ermenegildo', 'Kiriko', 'D.va', 
+  'Ashe', 'Tracer', 'Larry', 'Ellie', 'Mencho', 'Joaquin', 'Goku', 'Kokun', 'Travis', 'Scott'
+];
+
+nombres.forEach(nombre => {
+  console.log(nombre);
+});
+
